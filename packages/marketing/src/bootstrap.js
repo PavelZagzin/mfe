68a93587f11ab9e42ce45c4c9d79580e795d58c0
PavelZagzin <zagzin@gmail.com>
2021-11-16 16:39:07 +0200

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// Development / isolation call mount
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through container - export mount
+export { mount };

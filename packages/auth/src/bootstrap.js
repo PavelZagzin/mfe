475b026ffbd4253cd62a986d83d70ea9b721f261
PavelZagzin <zagzin@gmail.com>
2021-11-18 16:56:30 +0200

    Added authentication

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,18 +4,15 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
+    defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
 
   if (onNavigate) {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
@@ -29,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // Development / isolation call mount
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
